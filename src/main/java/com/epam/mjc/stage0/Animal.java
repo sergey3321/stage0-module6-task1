d3a490420b74b6789f5bc3ccb903cfd88e6cecde
sergey3321 <mail.sergey21@gmail.com>
2023-02-14 15:10:11 +0300

    stage0-module6-task1

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,55 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumberOfPaws() {
+        return numberOfPaws;
+    }
+
+    public void setNumberOfPaws(int numberOfPaws) {
+        this.numberOfPaws = numberOfPaws;
+    }
+
+    public boolean isHasFur() {
+        return hasFur;
+    }
+
+    public void setHasFur(boolean hasFur) {
+        this.hasFur = hasFur;
+    }
+
+    public Animal() {
+
+    }
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    protected void getDescription(String color, int numberOfPaws, boolean hasFur) {
+        if (numberOfPaws == 1) {
+            System.out.println("This animal is mostly " + color + ". It has " + numberOfPaws + " paw and ('a'/'no' -> depends on value of" + hasFur + ") fur.");
+        } else {
+            System.out.println("This animal is mostly " + color + ". It has " + numberOfPaws + " paws and ('a'/'no' -> depends on value of" + hasFur + ") fur.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.getDescription("blue", 2, true);
+        Bird bird = new Bird();
+        bird.getDescription("brown", 4, false);
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,13 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal {
+    public Bird () {
+        super("blue", 2, false);
+    }
 
+    @Override
+    public void getDescription(String color, int numberOfPaws, boolean hasFur) {
+        super.getDescription(color, numberOfPaws, hasFur);
+        System.out.println("This animal is mostly blue. It has 2 paws and no fur. Moreover, it has 2 wings and can fly.");
+    }
 }
